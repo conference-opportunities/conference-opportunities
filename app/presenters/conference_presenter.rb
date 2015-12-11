dfb2d83ff816b1cfddcf9afbda6572f108c72f54
Damien Burke and Doc Ritezel <pair+damien+doc@ministryofvelocity.com>
2015-12-11 12:35:42 -0800

    Add ConferencePresenter

---
+++ app/presenters/conference_presenter.rb
@@ -0,0 +1,13 @@
+class ConferencePresenter < Struct.new(:conference)
+  extend Forwardable
+  def_delegators :conference, :logo_url, :name, :website_url, :location,
+    :description
+
+  def twitter_name
+    "@#{conference.twitter_handle}"
+  end
+
+  def twitter_url
+    "https://twitter.com/#{conference.twitter_handle}"
+  end
+end
---
+++ spec/presenters/conference_presenter_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe ConferencePresenter do
+  let(:conference) do
+    Conference.new(
+      logo_url: 'http://example.com/logo.png',
+      name: 'Nameconf',
+      twitter_handle: 'nameconf',
+      location: 'Dystopia',
+      website_url: 'http://example.com',
+      description: 'A conference on names.'
+    )
+  end
+
+  subject(:presenter) { ConferencePresenter.new(conference) }
+
+  specify { expect(presenter.logo_url).to eq('http://example.com/logo.png') }
+  specify { expect(presenter.name).to eq('Nameconf') }
+  specify { expect(presenter.website_url).to eq('http://example.com') }
+  specify { expect(presenter.location).to eq('Dystopia') }
+  specify { expect(presenter.description).to eq('A conference on names.') }
+  specify { expect(presenter.twitter_name).to eq('@nameconf') }
+  specify { expect(presenter.twitter_url).to eq('https://twitter.com/nameconf') }
+end
