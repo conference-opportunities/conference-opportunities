1b0b03cf840cf44a7b6044d0a51fded9337b9f73
Damien Burke and Doc Ritezel <pair+damien+doc@ministryofvelocity.com>
2015-12-11 16:40:51 -0800

    Show tweets on conference pages

--- spec/features/view_a_conference_spec.rb
+++ spec/features/view_a_conference_spec.rb
@@ -1,17 +1,25 @@
 require "rails_helper"
 
-RSpec.feature "Conferences" do
+RSpec.feature "Conferences", :vcr, :js do
   let!(:conference) do
     Conference.create!(
-      name: "Interesing conference",
+      name: "Interesting conference",
       twitter_handle: "interestconf",
-      description: "About interesting things",
+      description: "All about how great oatmeal is, or something",
     )
   end
+  let(:actual_twitter_id) { '20' }
+  before do
+    conference.tweets.create!(twitter_id: actual_twitter_id)
+  end
 
-  scenario "Speaker views a conference" do
+  scenario "Speaker views a conference", :chrome do
     visit root_path
     click_on conference.name
     expect(page).to have_content conference.description
+    expect(page).to have_css '#twitter-widget-0'
+    within_frame('twitter-widget-0') do
+      expect(page).to have_content "just setting up my twttr"
+    end
   end
 end
