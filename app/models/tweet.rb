1db3ddb8abbcf3c9160ffadeef5de0ee303f2c4d
Doc Ritezel and Rae Bonfanti <pair+doc+rae@ministryofvelocity.com>
2015-12-19 17:02:00 -0800

    Validate that Tweet belongs to Conference

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,4 @@
 class Tweet < ActiveRecord::Base
   belongs_to :conference
+  validates :conference, presence: true
 end
--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Tweet do
+  let(:conference) { Conference.create!(twitter_handle: 'tweetconf') }
+  subject(:tweet) { conference.tweets.build(twitter_id: 1) }
+
   it { is_expected.to belong_to(:conference) }
+  it { is_expected.to validate_presence_of(:conference) }
 end
