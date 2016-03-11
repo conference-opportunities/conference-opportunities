f74c36f5a5eb953669dc6205dc1f3212667dc7a6
Damien Burke <pair+damien@ministryofvelocity.com>
2016-03-11 13:33:08 -0800

    Remove Conferences::ApprovalsController
    - Its functionality has been subsumed by the ConferencesController

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   devise_for :conference_organizers, controllers: {omniauth_callbacks: 'conference_organizers/omniauth_callbacks'}
-  resources :conferences, only: [:index, :show, :edit, :update] do
-    resource :approval, only: [:create], module: :conferences
-  end
+  resources :conferences, only: [:index, :show, :edit, :update]
   resource :approval, only: [:show]
   root to: 'conferences#index'
 end
