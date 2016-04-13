1641f402f2259ee3786d60e0b0b0881d1ec0425e
Chandrika Achar and Doc Ritezel <pair+chandrika+doc@ministryofvelocity.com>
2016-04-12 18:23:36 -0700

    Remove #edit/#update from Conference routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :organizers, controllers: {omniauth_callbacks: 'organizers/omniauth_callbacks'}
-  resources :conferences, only: [:index, :show, :edit, :update] do
+  resources :conferences, only: [:index, :show] do
     scope module: :conferences do
       resource :listing, only: [:new, :create]
       resource :detail, only: [:edit, :update]
