91ec6b5cd3b72b3051c7ccf9629ba314d4e4026c
Doc Ritezel <pair+doc@ministryofvelocity.com>
2016-05-07 18:46:51 -0700

    Add specs for locale detection and assignment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,17 @@ class ApplicationController < ActionController::Base
     current_organizer
   end
 
+  def current_locale
+    return params[:locale] if params.has_key?(:locale)
+    return current_organizer.locale if organizer_signed_in?
+    I18n.default_locale
+  end
+
   def set_locale
-    I18n.locale = params[:locale] if params.has_key?(:locale)
+    if organizer_signed_in?
+      current_organizer.assign_attributes(locale: current_locale)
+      current_organizer.save if current_organizer.changed?
+    end
+    I18n.locale = current_locale
   end
 end
