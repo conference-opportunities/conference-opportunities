7c345d7c9f573b3538f5488a756fc631961b4d2d
Doc Ritezel <pair+doc@ministryofvelocity.com>
2016-05-08 22:51:07 -0700

    Use locale_setter instead of writing it ourselves

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,6 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_action :set_locale
 
   def new_session_path(_scope)
     root_path
@@ -12,19 +11,5 @@ class ApplicationController < ActionController::Base
 
   def pundit_user
     current_organizer
-  end
-
-  def current_locale
-    return params[:locale] if params.has_key?(:locale)
-    return current_organizer.locale if organizer_signed_in?
-    I18n.default_locale.to_s
-  end
-
-  def set_locale
-    if organizer_signed_in?
-      current_organizer.assign_attributes(locale: current_locale)
-      current_organizer.save if current_organizer.changed?
-    end
-    I18n.locale = current_locale
   end
 end
