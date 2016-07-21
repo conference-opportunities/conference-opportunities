a5b0f8762c693b537d6177edc6a319190fb8b5d9
Doc Ritezel <pair+doc@ministryofvelocity.com>
2016-07-20 19:11:47 -0700

    Bump application.rb to Rails 4.2

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,14 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+require "rails"
+# Pick the frameworks you want:
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "sprockets/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -10,6 +18,8 @@ module ConferenceOpportunities
   class Application < Rails::Application
     config.autoload_paths << Rails.root.join('lib')
     config.autoload_paths << Rails.root.join('app/presenters')
+
+    # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.action_dispatch.rescue_responses["Pundit::NotAuthorizedError"] = :forbidden
 
