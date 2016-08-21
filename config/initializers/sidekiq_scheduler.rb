ccff150f43f483a7bffa84391d04b133afdcb98c
Doc Ritezel <pair+doc@ministryofvelocity.com>
2016-08-21 10:48:58 -0700

    Correctly configure scheduled job queues

--- config/initializers/sidekiq_scheduler.rb
+++ config/initializers/sidekiq_scheduler.rb
@@ -2,7 +2,7 @@ require 'sidekiq/scheduler'
 
 Sidekiq.configure_server do |config|
   config.on(:startup) do
-    Sidekiq.schedule = YAML.load_file(Rails.root.join('config/scheduler.yml'))[Rails.env]
+    Sidekiq.schedule = YAML.load(ERB.new(Rails.root.join('config/scheduler.yml').read).result)[Rails.env]
     Sidekiq::Scheduler.reload_schedule!
   end
 end
